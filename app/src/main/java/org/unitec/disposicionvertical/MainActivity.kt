3791411004c1a96362bf27ca6ac4838c06237b8b
campitos <rapidclimate@gmail.com>
2019-02-11 16:17:18 -0600

    la tercera es la vencida

--- app/src/main/java/org/unitec/disposicionvertical/MainActivity.kt
+++ app/src/main/java/org/unitec/disposicionvertical/MainActivity.kt
@@ -1,9 +1,11 @@
 package org.unitec.disposicionvertical
 
 import android.content.Context
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Vibrator
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +17,16 @@ class MainActivity : AppCompatActivity() {
         //Vamos a invocar el servicio del vibradordddd
      var vibrador=    getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
         vibrador.vibrate(3000)
+
+        //Las siguientes lineas de codigo sirven para navegar
+        //de esta activity a las del menu
+          navegar.setOnClickListener {
+          // aqui se maneja la logica dl cliqueo del boton
+              var i=Intent(this,MenuActivity::class.java)
+              //la siguiente linea nos invoca la navegacion
+              startActivity(i)
+
+          }
 
     }
 }
